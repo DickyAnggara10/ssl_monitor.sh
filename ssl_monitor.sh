8246e1ee851a0943ba8121e50a6c061c49e7d7ce
DickyAnggara10 <45477703+DickyAnggara10@users.noreply.github.com>
2023-06-01 22:18:40 +0700

    ssl_monitor.sh

--- ssl_monitor.sh
+++ ssl_monitor.sh
@@ -1,32 +1,38 @@
+#!/bin/bash
 
-domains=("siasat.uksw.edu" "google.com" "codingcollective.com")
+# Daftar domain yang akan dipantau
+domains=("example.com" "google.com" "facebook.com")
 
-for domain in "${domains[@]}"
-do
+# Fungsi untuk memantau sertifikat SSL
+monitor_ssl() {
+  for domain in "${domains[@]}"; do
+    echo "Domain: $domain"
+    echo "------------------------------"
+    echo "Informasi Sertifikat SSL:"
+    echo "------------------------------"
 
-    output=$(echo | openssl s_client -servername $domain -connect $domain:443 2>/dev/null | openssl x509 -noout -text)
-    
+    # Mendapatkan informasi sertifikat SSL menggunakan perintah openssl
+    certificate_info=$(openssl s_client -connect "$domain":443 -servername "$domain" -showcerts 2>/dev/null | openssl x509 -noout -text)
 
-    if [[ $? -eq 0 ]]; then
-        # Ekstrak informasi yang relevan dari output
-        subject=$(echo "$output" | grep -A 1 "Subject:" | tail -n 1 | awk '{$1=$1;print}')
-        issuer=$(echo "$output" | grep -A 1 "Issuer:" | tail -n 1 | awk '{$1=$1;print}')
-        start_date=$(echo "$output" | grep -i "not before" | awk -F= '{print $2}')
-        end_date=$(echo "$output" | grep -i "not after" | awk -F= '{print $2}')
-        key_type=$(echo "$output" | grep -i "Public Key Type:" | awk '{$1=$1;print}')
-        provider=$(echo "$output" | grep -A 1 "CA Issuers" | tail -n 1 | awk '{$1=$1;print}')
-        
+    # Ekstrak dan tampilkan detail sertifikat
+    subject=$(echo "$certificate_info" | awk -F 'subject=' '/subject=/ {print $2}')
+    issuer=$(echo "$certificate_info" | awk -F 'issuer=' '/issuer=/ {print $2}')
+    validity=$(echo "$certificate_info" | awk -F 'notBefore=' '/notBefore=/ {print "Mulai: " $2} ; /notAfter=/ {print "Hingga: " $2}')
+    start_date=$(echo "$certificate_info" | awk -F 'notBefore=' '/notBefore=/ {print $2}')
+    end_date=$(echo "$certificate_info" | awk -F 'notAfter=' '/notAfter=/ {print $2}')
+    key_type=$(echo "$certificate_info" | awk '/Public Key Type:/ {print $4}')
 
-        echo "Domain: $domain"
-        echo "Subject: $subject"
-        echo "Issuer: $issuer"
-        echo "SSL Start Date: $start_date"
-        echo "SSL End Date: $end_date"
-        echo "Key Type: $key_type"
-        echo "Certificate Provider: $provider"
-        echo "---------------------------"
-    else
-        echo "Tidak dapat menghubungi domain: $domain"
-        echo "---------------------------"
-    fi
-done
+    echo "Subjek: $subject"
+    echo "Penerbit: $issuer"
+    echo "Validitas: $validity"
+    echo "Tanggal Mulai SSL: $start_date"
+    echo "Tanggal Berakhir SSL: $end_date"
+    echo "Tipe Kunci: $key_type"
+
+    echo "------------------------------"
+    echo
+  done
+}
+
+# Panggil fungsi monitor_ssl
+monitor_ssl
