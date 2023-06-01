52d8256cf05c79db0c4d351880c27a8b321d9754
DickyAnggara10 <45477703+DickyAnggara10@users.noreply.github.com>
2023-06-01 23:35:47 +0700

    ssl_monitor.sh

--- ssl_monitor.sh
+++ ssl_monitor.sh
@@ -1,21 +1,36 @@
+
 domains=("youtube.com" "google.com" "codingcollective.com")
+
 
 monitor_ssl() {
   for domain in "${domains[@]}"; do
     echo "Domain: $domain"
     echo "------------------------------"
-    echo "Informasi Sertifikat SSL:"
-    echo "------------------------------"
 
 
-    certificate_info=$(openssl s_client -connect "$domain":443 -servername "$domain" -showcerts </dev/null 2>/dev/null | openssl x509 -noout -text)
+    certificate_info=$(echo | openssl s_client -servername "$domain" -connect "$domain":443 2>/dev/null | openssl x509 -noout -text)
 
-    echo "$certificate_info"
+    if [[ -n "$certificate_info" ]]; then
+      echo "Informasi Sertifikat SSL:"
+      echo "------------------------------"
+      echo "$certificate_info"
+
+ 
+      expiry_date=$(echo "$certificate_info" | awk -F "Not After : " '{print $2}')
+      expiry_timestamp=$(date -d "$expiry_date" +%s)
+      current_timestamp=$(date +%s)
+      expiry_days=$(( (expiry_timestamp - current_timestamp) / 86400 ))
+
+  
+      echo "Tanggal Kedaluwarsa: $expiry_days hari"
+    else
+      echo "Sertifikat SSL tidak ditemukan."
+    fi
 
     echo "------------------------------"
     echo
   done
 }
 
-
+# Panggil fungsi monitor_ssl
 monitor_ssl
