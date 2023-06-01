1c330a45d781f77dd2c909840ad674b82ecd488f
DickyAnggara10 <45477703+DickyAnggara10@users.noreply.github.com>
2023-06-01 22:51:05 +0700

    ssl_monitor.sh

--- ssl_monitor.sh
+++ ssl_monitor.sh
@@ -12,23 +12,9 @@ monitor_ssl() {
     echo "------------------------------"
 
     # Mendapatkan informasi sertifikat SSL menggunakan perintah openssl
-   certificate_info=$(openssl s_client -connect "$domain":443 -servername "$domain" -showcerts </dev/null 2>/dev/null | openssl x509 -noout -text)
+    certificate_info=$(openssl s_client -connect "$domain":443 -servername "$domain" -showcerts </dev/null 2>/dev/null | openssl x509 -noout -text)
 
-
-    # Ekstrak dan tampilkan detail sertifikat
-    subject=$(echo "$certificate_info" | awk -F 'subject=' '/subject=/ {print $2}')
-    issuer=$(echo "$certificate_info" | awk -F 'issuer=' '/issuer=/ {print $2}')
-    validity=$(echo "$certificate_info" | awk -F 'notBefore=' '/notBefore=/ {print "Mulai: " $2} ; /notAfter=/ {print "Hingga: " $2}')
-    start_date=$(echo "$certificate_info" | awk -F 'notBefore=' '/notBefore=/ {print $2}')
-    end_date=$(echo "$certificate_info" | awk -F 'notAfter=' '/notAfter=/ {print $2}')
-    key_type=$(echo "$certificate_info" | awk '/Public Key Type:/ {print $4}')
-
-    echo "Subjek: $subject"
-    echo "Penerbit: $issuer"
-    echo "Validitas: $validity"
-    echo "Tanggal Mulai SSL: $start_date"
-    echo "Tanggal Berakhir SSL: $end_date"
-    echo "Tipe Kunci: $key_type"
+    echo "$certificate_info"
 
     echo "------------------------------"
     echo
